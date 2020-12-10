439b0a9a1f4888c34fd35dbb3557c6b7b89f75ef
b15982003 <systex@jingchengsuixiangdeMacBook-Air.local>
2020-12-10 11:45:12 +0800

    master

--- app/src/main/java/com/example/recycleviewmaster/MainActivity.kt
+++ app/src/main/java/com/example/recycleviewmaster/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        
         val myItemList = arrayListOf<Item>()
 
         //生成資料
